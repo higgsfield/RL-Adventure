e97398d86a56c7e3bf25da85141a9ec075b56d08
sjsong <tenoclo@khu.ac.kr>
2018-06-26 21:39:47 +0900

    Noisy 동작안함

--- test/test.py
+++ test/test.py
@@ -41,6 +41,7 @@ class CnnDQN(nn.Module):
     )
 
   def forward(self, x):
+    x /= 255.0
     x = self.features(x)
     x = x.view(x.size(0), -1)
     x = self.fc(x)
@@ -59,14 +60,14 @@ class CnnDQN(nn.Module):
 
 
 env_id  = "DemonAttack-v0"
-DIR     = "../boltzmann"
+DIR     = "../epsilon"
 EPISODE = 500
 env     = make_atari(env_id)
-env     = wrap_deepmind(env, clip_rewards=False)
+env     = wrap_deepmind(env, clip_rewards=True)
 env     = wrap_pytorch(env)
 
 model_dir = os.path.join(DIR, "model")
-model_name = 'boltzmann' + "_" + env_id
+model_name = 'epsilon' + "_" + env_id
 
 model = load_model(model_dir, model_name)
 if USE_CUDA:
